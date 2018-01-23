da8dd2ceeded8fde3b21e3ee155c49d0852bd1d6
Dima Koval <dickjustice228@gmail.com>
2018-01-23 22:36:11 +0200

    Simple mock test added

---
+++ src/main/test/ua/training/dao/impl/JDBCArticleDaoTest.java
@@ -0,0 +1,38 @@
+package ua.training.dao.impl;
+
+import org.junit.Test;
+import ua.training.dao.ArticleDao;
+import ua.training.dao.factory.DaoFactory;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+import static org.junit.Assert.*;
+import static org.mockito.Matchers.anyInt;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.mockito.internal.verification.VerificationModeFactory.times;
+
+public class JDBCArticleDaoTest {
+    @Test
+    public void findAll() throws Exception {
+        Connection connection = mock(Connection.class);
+        Statement statement = mock(PreparedStatement.class);
+        ResultSet resultSet = mock(ResultSet.class);
+        when(connection.createStatement()).thenReturn(statement);
+        when(statement.executeQuery(anyString())).thenReturn(resultSet);
+
+
+        ArticleDao articleDao = new JDBCArticleDao(connection);
+        articleDao.findAll();
+        verify(connection, times(1)).createStatement();
+        verify(statement, times(1)).executeQuery(anyString());
+
+
+    }
+
+}
